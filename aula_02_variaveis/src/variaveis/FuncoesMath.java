7cd24e80c8ad65d6ffb445977d03c704d62d8f06
Marcos Willian <marcoswms@gmail.com>
2025-03-14 15:11:40 -0300

    Adicionado diretórios excluídos indevidamente

---
+++ aula_02_variaveis/src/variaveis/FuncoesMath.java
@@ -0,0 +1,17 @@
+package variaveis;//Pacote onde se encontra cada este código
+
+public class FuncoesMath {
+
+    //Revisão Função
+    public static void main(String[] args) {
+        double potencia = Math.pow(2, 3);
+        System.out.println(potencia);
+
+        double raizQuadrada = Math.sqrt(16);
+        System.out.println(raizQuadrada);
+
+        double numeroAleatorio = Math.random();
+        System.out.println(numeroAleatorio);
+    }
+
+}
